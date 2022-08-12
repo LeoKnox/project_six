21f7611f6e1d508feb6d5931b8910c9575df28bf
LeoKnox <LeoKnox@users.noreply.github.com>
2022-08-11 22:47:54 -0700

    Add files via upload

--- todo_api/models.py
+++ todo_api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Todo(models.Model):
+    task = models.CharField(max_length = 180)
+    timestamp = models.DateTimeField(auto_now_add = True, auto-now = False, blank = True)
+    completed = models.BoolenField(default = False, blank = True)
+    updated = models.DateTimeField(auto_now = True, blank = True)
+    user = models.ForeignKey(User, on_delete = models.CASCADE, blank = True, null = True)
+
+    def __str__(self):
+        return self.task
