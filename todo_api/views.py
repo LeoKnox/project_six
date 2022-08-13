e87c4c274ff010e58486eb97eab21bd001b15261
LeoKnox <LeoKnox@users.noreply.github.com>
2022-08-12 23:12:23 -0700

    Add files via upload

--- todo_api/views.py
+++ todo_api/views.py
@@ -5,6 +5,51 @@ from rest_framework import permissions
 from .models import Todo
 from .serializers import TodoSerializer
 
+class TodoDetailApiView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self, todo_id, user_id):
+        try:
+            return Todo.objects.get(id=todo_id, user = user_id)
+        except Todo.DoesNotExist:
+            return None
+
+    def get(self, request, *args, **kwargs):
+        todos = Todo.objects.filter(user = request.user.id)
+        serializer = TodoSerializer(todos, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def put(self, request, todo_id, *args, **kwargs):
+        todo_instance = self.get_object(todo_id, request.user.id)
+        if not todo_instance:
+            return Response(
+                {"res": "object with todo id does not exist"},
+                status = status.HTTP_400_BAD_REQUEST
+            )
+        data = {
+            'task': request.data.get('task'),
+            'completed': request.data.get('completed'),
+            'user': request.user.id
+        }
+        serializer = TodoSerializer(instance = todo_instance, data=data, partial = True)
+        if seriailizer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, todo_id, *args, **kwargs):
+        todo_instance = self.get_object(todo_id, request.user.id)
+        if not todo_instance:
+            return Response(
+                {"res": "object with todo id does not exist"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        todo_instance.delete()
+        return Response(
+            {"res": "Object deleted"},
+            status=status.HTTP_200_OK
+        )
+
 class TodoListApiView(APIView):
     permission_classes = [permissions.IsAuthenticated]
 
@@ -12,7 +57,7 @@ class TodoListApiView(APIView):
         todos = Todo.objects.filter(user = request.user.id)
         serializer = TodoSerializer(todos, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
-    
+
     def post(self, request, *args, **kwargs):
         data = {
             'task': request.data.get('task'),
