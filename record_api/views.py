f3e519082dc29383021b4e28d7d4ed0669f2fac1
LeoKnox <LeoKnox@users.noreply.github.com>
2022-08-13 22:53:30 -0700

    Add files via upload

--- record_api/views.py
+++ record_api/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework import permissions
+from .models import Record
+from .serializers import RecordSerializer
 
-# Create your views here.
+class RecordDetailApiView(APIView):
+    return ("record_detail")
+
+class RecordListApiView(APIView):
+    return("record_list")
