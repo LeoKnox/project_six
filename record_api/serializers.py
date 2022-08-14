0c5cc724ca4f56a6677aa8e786a7e99b3a6d6127
LeoKnox <LeoKnox@users.noreply.github.com>
2022-08-13 23:08:04 -0700

    Add files via upload

--- record_api/serializers.py
+++ record_api/serializers.py
@@ -1,7 +1,7 @@
  from rest_framework import serializers
  from .models import Record
 
- class TodoSerializer(serializers.ModelSerializer):
+ class RecordSerializer(serializers.ModelSerializer):
      class Meta:
          model = Record
          fields = ["record"]
