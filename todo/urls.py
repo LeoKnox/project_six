a47335aac7cb91b1f3909992ed2bce26dd66bb02
LeoKnox <LeoKnox@users.noreply.github.com>
2022-08-13 22:42:25 -0700

    Add files via upload

--- todo/urls.py
+++ todo/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from record_api import urls as record_urls
 from todo_api import urls as todo_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('todos/', include(todo_urls)),
+    path('record/', include(record_urls)),
 ]
