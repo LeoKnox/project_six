20f500d956be1183e2b3cd46c366ad5b9346bd1e
LeoKnox <LeoKnox@users.noreply.github.com>
2022-08-19 22:13:24 -0700

    Add files via upload

--- todo/settings.py
+++ todo/settings.py
@@ -37,12 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'record_api',
     'todo_api',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -130,15 +132,3 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     )
 }
-
-CORS_ALLOW_METHODS = [
-    'DELETE',
-    'GET',
-    'PUT',
-    'POST',
-]
-
-CORS_ALLOW_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:8000",
-]
