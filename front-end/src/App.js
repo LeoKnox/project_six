d674f0d8565d54f83674e1d4c580e7b23708b349
LeoKnox <LeoKnox@users.noreply.github.com>
2022-08-19 18:55:33 -0700

    Add files via upload

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,14 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "React";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Display Records</h1>
-      </header>
-    </div>
-  );
+const App = () => {
+  const [records, setRecord] = useState(null);
+  
+  useEffect(() => {
+    fetch('http://127.0.0.1:8000/record/api/')
+      .then(console.log("a"))
+  }, []);
+
+  function App() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Display Records</h1>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
