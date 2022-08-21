01d322a2ecc3a9493282ee2a003fffdf4ca01e7c
LeoKnox <LeoKnox@users.noreply.github.com>
2022-08-20 23:26:55 -0700

    Add files via upload

--- todo/settings.py
+++ todo/settings.py
@@ -131,6 +131,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    )
 }
 
 CORS_ALLOW_ALL_ORIGINS = True
