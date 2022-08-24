2845c3886ff970e12b7261fe49906ee426a4ec7f
LeoKnox <LeoKnox@users.noreply.github.com>
2022-08-23 23:03:06 -0700

    Add files via upload

--- front-end/src/single.js
+++ front-end/src/single.js
@@ -3,8 +3,10 @@ import React from 'react';
 
 function Single(id) {
     useEffect(() => {
-        console.log(`http://127.0.0.1:8000/record/api/{id.id}`)
+        fetch(`http://127.0.0.1:8000/record/api/${id.id}`)
+            .then((response) => response.json())
         console.log("RED RED DER " + JSON.stringify(id));
+        console.log(response);
     })
 
     return (
