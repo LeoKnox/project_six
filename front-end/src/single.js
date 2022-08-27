0ea60ec6e4aea50572bca9d644379f69abb31a14
LeoKnox <LeoKnox@users.noreply.github.com>
2022-08-26 22:22:50 -0700

    Add files via upload

--- front-end/src/single.js
+++ front-end/src/single.js
@@ -1,21 +1,10 @@
 import { useEffect,useState } from 'react';
 import React from 'react';
 
-function Single(id) {
-    const [single, setSingle] = useState(null);
-
-    useEffect(() => {
-        fetch(`http://127.0.0.1:8000/record/api/${id.id}`)
-            .then((response) => response.json())
-            .then(setSingle)
-            .catch((err) => {
-                console.log(err.message);
-            })
-        console.log("RED RED DER " + JSON.stringify(id));
-    }, [])
+function Single(record) {
 
     return (
-        <p>One at a time</p>
+        <p>One at a time {record.record}</p>
     )
 }
 
