e1d82d8e25bf46beb8018ba14d1a1f3536ab4d72
LeoKnox <LeoKnox@users.noreply.github.com>
2022-08-29 22:08:06 -0700

    Add files via upload

--- front-end/src/single.js
+++ front-end/src/single.js
@@ -2,10 +2,10 @@ import { useEffect,useState } from 'react';
 import React from 'react';
 
 function Single(record) {
-    console.log("***"+record);
+    console.log("***"+{record});
 
     return (
-        <p>One at a time</p>
+        <p>One at a time {record}</p>
     )
 }
 
